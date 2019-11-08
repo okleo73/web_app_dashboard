bc53ab20af6c98ccc06a3b779df2b6d7b9328dcc
Clay <bigafro3@gmail.com>
2019-11-07 22:46:13 -0500

    updated styles

--- js/app.js
+++ js/app.js
@@ -1,5 +1,17 @@
-let alert3 = document.getElementById('alert2');
+//variables
+const months = ['Oct', 'Nov', 'Dec', 'Jan', 'Feb'];
+const monthlyPerformance = [1200, 1300, 1400, 1500, 1600];
+const week = ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31'];
+const weeklyPerformance = [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500];
+const days = ["Oct 10", "Oct 11", "Oct 12", "Oct 13", "Oct 14", "Oct 15", "Oct 16", "Oct 17", "Oct 18", "Oct 19", "Oct 20", "Oct 21", "Oct 22", "Oct 23"];
+const dailyPerformance = [359, 497, 267, 127, 247, 348, 408, 598, 100, 180, 490, 80, 403, 809];
+const hours = ['10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm'];
+const hourPerformance = [50, 80, 100, 98, 189, 39, 70, 89, 25]; 
+//End variable section
 
+
+//Banner alert Section
+let alert3 = document.getElementById('alert2');
 alert3.innerHTML = 
     `<div class="alert-banner">
         <p><strong>Alert:</strong>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
@@ -11,11 +23,13 @@ alert3.addEventListener('click', e => {
         alert3.style.display= 'none'
     }
 });
+//End banner alert section
 
+
+//Message user section
 const userName = document.getElementById('user-field');
 const userMessage = document.getElementById('messages');
 const sendBttn = document.getElementById('send');
-
 sendBttn.addEventListener('click', () => {
     if(userName.value === "" && userMessage.value == "") {
         alert("Please input a valid message and user name.");
@@ -25,6 +39,7 @@ sendBttn.addEventListener('click', () => {
         alert(`Message was successfully sent to ${userName.value}!`);
     }
 });
+//End message user section
 
 
 //Not working. Suppose to add class to active navigation
@@ -38,12 +53,12 @@ const activeNavBttn = () => {
     let navPath = document.querySelector('.nav-paths');
     navPath.classList.add("active-nav-bttn");
 }
+//End of Navigation button section
 
 
 var ctx = document.getElementById('traffic-chart');
 var ctx2 = document.getElementById('daily-chart');
 var ctx3 = document.getElementById('mobile-chart');
-
 var myChart = new Chart(ctx, {
     type: 'line',
     data: {
@@ -52,34 +67,36 @@ var myChart = new Chart(ctx, {
             label: '# of Votes',
             data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)',
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)'
+                
+                'rgba(116, 119, 191, 0.3)'
             ],
-            // borderColor: [
-            //     'rgba(255, 99, 132, 1)',
-            //     'rgba(54, 162, 235, 1)',
-            //     'rgba(255, 206, 86, 1)',
-            //     'rgba(75, 192, 192, 1)',
-            //     'rgba(153, 102, 255, 1)',
-            //     'rgba(255, 159, 64, 1)',
-            //     'rgba(255, 99, 132, 1)',
-            //     'rgba(54, 162, 235, 1)',
-            //     'rgba(255, 206, 86, 1)',
-            //     'rgba(75, 192, 192, 1)',
-            //     'rgba(153, 102, 255, 1)'
-            // ],
-            borderWidth: 1
+            borderColor: [
+                'rgba(116, 119, 191, 1)'
+            ],
+            borderWidth: 2,
+            lineTension: 0 
         }]
     },
+    // tooltips: {    
+    //     callbacks: {
+    //         label: function (tooltipItems, data) {
+    //             return '$' +tooltipItems.yLabel;
+    //         }
+    //     }
+    // },
+    // responsive: true, 
+    // scale: {
+    //     yAxes: [{
+    //         ticks: {
+    //             beginAtZero: true,
+    //             callback: function(value, index, values) {
+    //                 return '$' + value;
+    //             },
+    //             autoSkip: true,
+    //             maxTicksLimit: 8
+    //         }
+    //     }]
+    // }
     options: {
         scales: {
             yAxes: [{
@@ -90,20 +107,47 @@ var myChart = new Chart(ctx, {
         }
     }
 });
-// let trafficOptions {
-//     aspectRatio: 2.5,
-//     animation: {
-//         duration: 0
+
+//Event listener for when a buttons are clicked
+// let lineGraphNav = document.querySelector('.trafic-nav');
+// let lineGraphLI = lineGraphNav.childNodes;
+// lineGraphNav.addEventListener('click', (e) => {
+//     const navText = e.target.textContent;
+//     for (let i = 0; i < lineGraphLI.length; i++) {
+//         lineGraphLI[i].className = "";
 //     }
-//     legend {
-//         display: none;
+//     e.target.className = "active";
+//     if (navText == "Hourly") {
+//         myChart.data.labels = 0;
+//         myChart.data.labels = [...hours];
+//         myChart.data.datasets.forEach((dataset) => {
+//             dataset.data = [...hourPerformance];
+//         });
+//     } else if (navText == "Daily") {
+//         myChart.data.labels = 0;
+//         myChart.data.labels = [...days]
+//         myChart.data.datasets.forEach((dataset) => {
+//             dataset.data = [...dailyPerformance];
+//         });
+//     } else if (navText == "Weekly") {
+//         myChart.data.labels = 0;
+//         myChart.data.labels = [...week]
+//         myChart.data.datasets.forEach((dataset) => {
+//             dataset.data = [...weeklyPerformance];
+//         });
+//     } else {
+//         myChart.data.labels = 0;
+//         myChart.data.labels = [...months]
+//         myChart.data.datasets.forEach((dataset) => {
+//             dataset.data = [...monthlyPerformance];
+//         });
 //     }
-// }
-// let trafficChart = new Chart(trafficCanvas, {
-//     type: 'line',
-//     display: myChart,
-//     options: trafficOptions
+
+//     myChart.update();
 // });
+//End event-listener code
+
+
 var myChart2 = new Chart(ctx2, {
     type: 'bar',
     data: {
@@ -112,22 +156,16 @@ var myChart2 = new Chart(ctx2, {
             label: '# of Votes',
             data: [75, 115, 175, 125, 225, 200, 100],
             backgroundColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)',
-                'rgba(255, 159, 64, 1)'
+                "#7477bf",
+                "#7477bf",
+                "#7477bf",
+                "#7477bf",
+                "#7477bf",
+                "#7477bf",
+                "#7477bf",
             ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)',
-                'rgba(255, 159, 64, 1)'
+                
             ],
             borderWidth: 1
         }]
@@ -142,6 +180,7 @@ var myChart2 = new Chart(ctx2, {
         }
     }
 });
+
 var myChart3 = new Chart(ctx3, {
     type: 'doughnut',
     data: {
@@ -150,21 +189,11 @@ var myChart3 = new Chart(ctx3, {
             label: '# of Votes',
             data: [2000, 550, 500],
             backgroundColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                '#7477BF',
+                '#78CF82',
+                '#51B6C8'
             ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
+            borderColor: [ ],
             borderWidth: 1
         }]
     },
